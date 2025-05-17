3340d10353e33d3642ac80e7201b1f0c37a8a0e0
Karan Jagdish <karanjagdish@gmail.com>
2025-05-17 15:48:49 +0530

    added redux dev tool support

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-
-    console.log("Type: ", action.type);
-    console.log("Payload: ", action.payload);
-    console.log("Current State: ", store.getState());
-
-    next(action);
-
-    console.log("Next state: ", store.getState());
-};
 
 // root-reducer
 
@@ -29,9 +15,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+    Boolean
+);
 
-const composeEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+    (process.env.NODE_ENV !== "production" &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
 export const persistor = persistStore(store);
